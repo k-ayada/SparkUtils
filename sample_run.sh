3e20d3071244be530593fd004dbbb6692c18730b
k-ayada <kiran14n@gmail.com>
2017-05-26 07:45:24 +0530

    Update sample_run.sh

--- sample_run.sh
+++ sample_run.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+numExe=7
+exeCores=5
+exeMem=10GB
+driCores=4
+drivMem=4GB
+src=prod
+
+export HADOOP_OPTS="$HADOOP_OPTS -Dhttps.protocols=TLSv1.1,TLSv1.2 -Djava.library.path=/opt/cloudera/parcels/CDH-5.8.2-1.cdh5.8.2.p0.3/lib/hadoop/lib/native"
+export CLASSPATH="$CLASSPATH:/hadoop/jars/*:/opt/cloudera/parcels/CDH-5.8.2-1.cdh5.8.2.p0.3/jars/ojdbc6.jar"
+
+rc=0
+echo `date +"%Y%m%d %H:%M:%S"` StartingScript
+
 spark-submit -v --name "jdbc2Hive-${src}"  \
 --class pub.ayada.scala.sparkUtils.HiveLoad \
 --master yarn --deploy-mode cluster \
@@ -9,3 +24,7 @@ spark-submit -v --name "jdbc2Hive-${src}"  \
 --jars /opt/cloudera/parcels/CDH-5.8.2-1.cdh5.8.2.p0.3/jars/mysql-connector-java.jar,/opt/cloudera/parcels/CDH-5.8.2-1.cdh5.8.2.p0.3/jars/ojdbc6.jar \
 /hadoop/jars/SparkUtils-0.1-jar-with-dependencies.jar \
 "jdbc2Hive" --JobConf "hdfs:///user/k-ayada/${src}.xml" >./${src}.log 2>&1
+
+rc=$?
+
+echo `date +"%Y%m%d %H:%M:%S"` Ending Script RC:$rc
